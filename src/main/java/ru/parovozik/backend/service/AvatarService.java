9f75d4e6b6e96b0d9f6c78ebe3a85cc7761caa9b
nanapap1 <markmoskvitin1007@gmail.com>
2026-05-01 21:27:09 +0300

    Add avatar service

---
+++ src/main/java/ru/parovozik/backend/service/AvatarService.java
@@ -0,0 +1,26 @@
+package ru.parovozik.backend.service;
+
+import org.springframework.stereotype.Service;
+import ru.parovozik.backend.entity.Avatar;
+import ru.parovozik.backend.repostitory.AvatarRepository;
+
+@Service
+public class AvatarService {
+    private final AvatarRepository avatarRepository;
+
+    public AvatarService(AvatarRepository avatarRepository) {
+        this.avatarRepository = avatarRepository;
+    }
+
+    public boolean create(String link) {
+        try {
+            Avatar avatar = new Avatar();
+            avatar.setLink(link);
+            avatarRepository.save(avatar);
+            return true;
+        }
+        catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+}
