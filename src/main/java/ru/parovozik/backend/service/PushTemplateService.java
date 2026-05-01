6c3ac7280a9b1e865b1e19c4d6929e9224efe6da
nanapap1 <markmoskvitin1007@gmail.com>
2026-05-01 21:33:51 +0300

    Add pushtemplate service

---
+++ src/main/java/ru/parovozik/backend/service/PushTemplateService.java
@@ -0,0 +1,39 @@
+package ru.parovozik.backend.service;
+
+import org.springframework.stereotype.Service;
+import ru.parovozik.backend.entity.PushTemplate;
+import ru.parovozik.backend.repostitory.PushTemplateRepository;
+
+import java.time.Duration;
+import java.time.Period;
+import java.util.NoSuchElementException;
+
+@Service
+public class PushTemplateService {
+    private final PushTemplateRepository pushTemplateRepository;
+
+    public PushTemplateService(PushTemplateRepository pushTemplateRepository) {
+        this.pushTemplateRepository = pushTemplateRepository;
+    }
+
+    public boolean create(Period period, Duration duration) {
+        try {
+            PushTemplate pushTemplate = new PushTemplate();
+            pushTemplate.setBeforeHowDays(period);
+            pushTemplate.setBeforeHowHours(duration);
+            pushTemplateRepository.save(pushTemplate);
+            return true;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public PushTemplate getByPeriodAndDuration(Period period, Duration duration) {
+        try {
+            PushTemplate pushTemplate = pushTemplateRepository.findPushTemplateByBeforeHowDaysAndBeforeHowHours(period,duration);
+            return pushTemplate;
+        } catch (Exception e) {
+            throw new NoSuchElementException(e);
+        }
+    }
+}
